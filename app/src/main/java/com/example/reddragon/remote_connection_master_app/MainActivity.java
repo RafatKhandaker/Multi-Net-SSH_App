98b6f50e678b949f15b92c41fd995b61d94ec2cc
RafatK <Rafat.Khandaker@gmail.com>
2017-02-07 13:54:42 -0500

    still thinking UI Design

--- app/src/main/java/com/example/reddragon/remote_connection_master_app/MainActivity.java
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.reddragon.remote_connection_master_app;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
-/** written by Rafat Khandaker
- *                                          02/06/17
- * Plan : everything is written around the android UI thread as the central core of the application
- *
- *
- * **/
+import com.example.reddragon.remote_connection_master_app.View.ConSettingsView;
+import com.example.reddragon.remote_connection_master_app.View.ConsoleView;
 
-public class MainActivity extends AppCompatActivity {
+/**------------------------------------------------------------------------------------------------->
+ * Plan: Everything is written around the android UI thread as the central core of the application /
+ *                                                                                                 \
+ *                                                                    written: by,                 /
+ *                                                                                 Rafat Khandaker \
+ *                                                                                                 /
+ *                                                                                        02/06/17 \
+ * ----------------------------------------------------------------------------------------------**/
 
-LinearLayout connectionSettings;
-LinearLayout console;
+public class MainActivity extends FragmentActivity {
+
+private LinearLayout connectionSettings;
+private LinearLayout console;
+
+private static final Fragment CONNECTION_SETTINGS = new ConSettingsView();
+private static final ConsoleView CONSOLE = new ConsoleView();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +38,41 @@ LinearLayout console;
         connectionSettings = new LinearLayout(this);
         console = new LinearLayout(this);
 
-
     }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+
+            case R.id.settings_menu:
+                launchContainer(CONNECTION_SETTINGS);
+
+                return true;
+
+            case R.id.console_menu:
+                launchContainer(CONSOLE);
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void launchContainer(Fragment fragment){
+        FragmentManager fragMan = getSupportFragmentManager();
+        FragmentTransaction fragTrans = fragMan.beginTransaction();
+        fragTrans.add(R.id.mainframe_container, fragment);
+        fragTrans.commit();
+    }
+
 
 }
---
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/View/ConsoleView.java
@@ -0,0 +1,11 @@
+package com.example.reddragon.remote_connection_master_app.View;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by RedDragon on 2/7/17.
+ */
+
+public class ConsoleView extends Fragment {
+}
