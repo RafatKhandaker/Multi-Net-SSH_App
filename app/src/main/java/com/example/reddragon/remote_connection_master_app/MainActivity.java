c3daaa80ca558470cba7266418331f1708fadb22
RafatK <Rafat.Khandaker@gmail.com>
2017-02-11 15:52:57 -0500

    initated drawer sliding... works but has bugs, only small point of slider

--- app/src/main/java/com/example/reddragon/remote_connection_master_app/MainActivity.java
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.reddragon.remote_connection_master_app;
 
+import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -55,7 +56,7 @@ private static final ConsoleView CONSOLE = new ConsoleView();
     private StringBuffer bufferValue;
 
 
-    private ActionBarDrawerToggle drawerToggle;
+    private android.support.v4.app.ActionBarDrawerToggle drawerToggle;
     private ListView drawerList;
     String[] andoridVeriosnArray;
 
@@ -101,6 +102,12 @@ private static final ConsoleView CONSOLE = new ConsoleView();
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
 
@@ -116,6 +123,21 @@ private static final ConsoleView CONSOLE = new ConsoleView();
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
 
 
     private void initiateSettingsClick(View v){
@@ -140,11 +162,14 @@ private static final ConsoleView CONSOLE = new ConsoleView();
         drawerList.setAdapter(new ArrayAdapter<>(MainActivity.this,
                 R.layout.drawer_list_item, andoridVeriosnArray));
 
-//        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+//        getActionBar().setHomeButtonEnabled(true);
+
 
         drawerToggle = new ActionBarDrawerToggle(
-                this, drawerLayout, null, R.string.drawer_open, R.string.drawer_close)
-        {
+                this, drawerLayout, R.drawable.profile_icon, R.string.drawer_open, R.string.drawer_close) {
             public void onDrawerClosed(View view) {
                 invalidateOptionsMenu();
             }
@@ -153,15 +178,29 @@ private static final ConsoleView CONSOLE = new ConsoleView();
                 invalidateOptionsMenu();
             }
         };
+
+        drawerToggle = new android.support.v4.app.ActionBarDrawerToggle(
+                this, drawerLayout, R.drawable.profile_icon, R.string.drawer_open, R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
         drawerLayout.setDrawerListener(drawerToggle);
 
     }
 
     private void launchContainer(Fragment fragment){
         FragmentManager fragMan = getSupportFragmentManager();
-        FragmentTransaction fragTrans = fragMan.beginTransaction();
-        fragTrans.add(R.id.mainframe_container, fragment);
-        fragTrans.commit();
+         fragMan.beginTransaction()
+                .replace(R.id.mainframe_container, fragment)
+                .commit();
     }
 
     private void loadSavedData(){
@@ -173,6 +212,17 @@ private static final ConsoleView CONSOLE = new ConsoleView();
             }
         }
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem();
+        }
+
+        private void selectItem(){}
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/reddragon/remote_connection_master_app/View/FrameFragments/ConsoleView.java
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/View/FrameFragments/ConsoleView.java
@@ -1,11 +1,22 @@
 package com.example.reddragon.remote_connection_master_app.View.FrameFragments;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.reddragon.remote_connection_master_app.R;
 
 /**
  * Created by RedDragon on 2/7/17.
  */
 
 public class ConsoleView extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.console, container, false);
+    }
 }
