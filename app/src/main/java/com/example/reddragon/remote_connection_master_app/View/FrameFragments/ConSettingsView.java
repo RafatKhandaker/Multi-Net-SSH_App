f29bf10c7db7366c9a2d6f70a6ab3fedd4818618
RafatK <Rafat.Khandaker@gmail.com>
2017-02-14 19:09:09 -0500

    update profile folder

--- app/src/main/java/com/example/reddragon/remote_connection_master_app/View/FrameFragments/ConSettingsView.java
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/View/FrameFragments/ConSettingsView.java
@@ -17,6 +17,6 @@ public class ConSettingsView extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.connection_settings, container, false);
+        return inflater.inflate(R.layout.secure_folder, container, false);
     }
 }
