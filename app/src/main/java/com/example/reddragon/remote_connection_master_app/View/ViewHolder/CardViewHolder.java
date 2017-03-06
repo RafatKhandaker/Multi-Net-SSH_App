d508bd28341ccd4c5f7ece42f2ce33b72b921bbe
RafatK <Rafat.Khandaker@gmail.com>
2017-03-06 08:29:10 -0500

    update

--- app/src/main/java/com/example/reddragon/remote_connection_master_app/View/ViewHolder/CardViewHolder.java
+++ app/src/main/java/com/example/reddragon/remote_connection_master_app/View/ViewHolder/CardViewHolder.java
@@ -2,8 +2,10 @@ package com.example.reddragon.remote_connection_master_app.View.ViewHolder;
 
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.reddragon.remote_connection_master_app.R;
 
@@ -16,10 +18,31 @@ import java.util.ArrayList;
 public class CardViewHolder extends RecyclerView.ViewHolder {
 
     private EditText editHostName;
+    private ImageButton editHostButton;
 
     public CardViewHolder(View itemView) {
         super(itemView);
+
         editHostName = (EditText)itemView.findViewById(R.id.hostname_et);
+        editHostButton = (ImageButton)itemView.findViewById(R.id.edit_hostname_btn);
+
+        editHostName.setEnabled(false);
+
+        editHostButton.setOnTouchListener(new ImageButton.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editHostName.setEnabled(true);
+                editHostName.requestFocus();
+                return false;
+            }
+        });
+
+        editHostName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                v.setEnabled(false);
+            }
+        });
     }
 
     public void setHostName(int pos){
